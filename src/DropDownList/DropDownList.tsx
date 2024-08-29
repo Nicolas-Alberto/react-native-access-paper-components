44f48e4712689ffcbf5fe9428f58e9fe1210c9d0
Ruy Menezes <ruy.menezes@accessbrasil.net.br>
2024-08-29 13:06:19 -0300

    Atualização no tamanho maximo e mudança do botão de fechar

--- src/DropDownList/DropDownList.tsx
+++ src/DropDownList/DropDownList.tsx
@@ -1,6 +1,6 @@
 import { useRef, useState } from "react";
 import React from "react";
-import { FlatList, TouchableOpacity } from "react-native";
+import { Dimensions, FlatList, TouchableOpacity, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Button, Dialog, Portal, TextInput, RadioButton, Divider } from "react-native-paper";
 
@@ -22,7 +22,7 @@ export type DropDownListPropsType = Omit<React.ComponentProps<typeof TextInput>,
 };
 
 export function DropDonwList({
-    style, label, placeholder, mode, value, setValue, options, optionId, optionNome, inicialLabel, error, select2
+    style, label, placeholder, mode, value, setValue, options, optionId, optionNome, inicialLabel, error, select2, multiline
 }: DropDownListPropsType) {
     const [visible, setVisible] = useState(false);
     const [textoBusca, setTextoBusca] = useState("");
@@ -56,47 +56,57 @@ export function DropDonwList({
         <React.Fragment>
 
             <TouchableOpacity onPress={() => setVisible(true)} >
-                <TextInput style={style} label={label} placeholder={placeholder} mode={mode ? mode : 'outlined'} value={getText()} editable={false} error={error} right={<TextInput.Icon icon="menu-down" onPress={() => setVisible(true)} />} />
+                <TextInput style={style} label={label} multiline={multiline ? multiline : false} placeholder={placeholder} mode={mode ? mode : 'outlined'} value={getText()} editable={false} error={error} right={<TextInput.Icon icon="menu-down" onPress={() => setVisible(true)} />} />
             </TouchableOpacity>
             <Portal>
 
-                <Dialog visible={visible} onDismiss={() => fecharDropDown()}>
+                <Dialog visible={visible} onDismiss={() => fecharDropDown()} style={{ maxHeight: Dimensions.get('window').height * 0.8 }}>
                     <GestureHandlerRootView >
-                        <Dialog.Title>Selecione a opção:</Dialog.Title>
+                        <Button style={{ margin: 0, padding: 0, alignItems: "flex-end" }} onPress={() => fecharDropDown()}>Fechar</Button>
+                        <Dialog.Title style={{ paddingTop: 0, marginTop: 0 }}>
+                            Selecione a opção:
+                        </Dialog.Title>
                         <Dialog.Content>
-
-                            {
-                                select2 &&
-                                <>
-                                    <TextInput label={"Busca"} placeholder="Busca" mode={mode ? mode : 'outlined'} value={textoBusca}
-                                        left={<TextInput.Icon icon={"magnify"} />}
-                                        onChangeText={(e) => buscarOpcao(e)}
-                                    />
-                                    <Divider />
-                                </>
-                            }
-                            <RadioButton.Group onValueChange={value => marcarOpcao(value)} value={value}>
+                            <View >
                                 {
-                                    inicialLabel !== undefined && textoBusca === "" && (
-                                        <RadioButton.Item value={""} label={inicialLabel} />
-                                    )
+                                    select2 &&
+                                    <>
+                                        <TextInput label={"Busca"} placeholder="Busca" mode={mode ? mode : 'outlined'} value={textoBusca}
+                                            left={<TextInput.Icon icon={"magnify"} />}
+                                            onChangeText={(e) => buscarOpcao(e)}
+                                        />
+                                        <Divider />
+                                    </>
                                 }
-                                <FlatList
-                                    data={options.filter(option => option[optionNome].toLowerCase().includes(textoBusca.toLowerCase()) || textoBusca === "")}
-                                    initialNumToRender={4}
-                                    renderItem={({ item, index}) => (
-                                        
-                                            <RadioButton.Item key={index} value={item[optionId]} label={item[optionNome]} />
-     
-                                    )}
-                                    ref={myListRef}
-                                />
-                            </RadioButton.Group>
+                                <RadioButton.Group onValueChange={value => marcarOpcao(value)} value={value}>
+                                    {
+                                        inicialLabel !== undefined && textoBusca === "" && (
+                                            <RadioButton.Item value={""} label={inicialLabel} />
+                                        )
+                                    }
+                                    {
+                                        options.length > 5 &&
+                                        <FlatList
+                                            style={{ maxHeight: Dimensions.get('window').height * 0.4}}
+                                            data={options.filter(option => option[optionNome].toLowerCase().includes(textoBusca.toLowerCase()) || textoBusca === "")}
+                                            initialNumToRender={2}
+                                            renderItem={({ item, index }) => (
 
+                                                <RadioButton.Item key={index} value={item[optionId]} label={item[optionNome]} />
+
+                                            )}
+                                            ref={myListRef}
+                                        />
+                                    }
+                                    {
+                                        options.length < 5 &&
+                                        options.filter(option => option[optionNome].toLowerCase().includes(textoBusca.toLowerCase()) || textoBusca === "").map((option: any, key: any) =>
+                                            <RadioButton.Item key={key} value={option[optionId]} label={option[optionNome]} />
+                                        )
+                                    }
+                                </RadioButton.Group>
+                            </View>
                         </Dialog.Content>
-                        <Dialog.Actions>
-                            <Button onPress={() => fecharDropDown()}>Fechar</Button>
-                        </Dialog.Actions>
 
                     </GestureHandlerRootView>
 
